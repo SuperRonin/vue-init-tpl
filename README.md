e41e208dd75b20cc44b90e8374179aa140be64a7
xuexingwei <xuexingwei@bus365.com>
2022-03-16 00:01:46 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vue 3 + Vite
+
+ The template can help you init a vue3 project quickly.
+
+
+
+## Quick start
+
+- npm install
+- npm run dev
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <div>X坐标：{{ x }} <br/>Y坐标:{{y}}</div>
+
+  <div class="mt-20px mb-20px">当前网络状态： {{ online }}</div>
+
+  <el-button type="primary">submit</el-button>
+
+  <el-date-picker v-model="dateVal" type="date" placeholder="Pick a day" />
+</template>
+
+
+<script setup>
+const { x, y } = useMouse();
+const online = useOnline();
+const dateVal = ref("");
+debugger
+</script>
+
+<style lang="scss">
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
